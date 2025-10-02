d91813abc2d2a04fd84fcec2c6942cf61bce390a
Eva Maleckova <41913488+emaleckova@users.noreply.github.com>
2025-10-02 18:21:18 +0200

    Reworked into a book format

---
+++ _setup.R
@@ -0,0 +1,14 @@
+# This is to load all required packages at start of rendering of each chapter
+# ===========================================================================
+
+# Data wrangling, functional programming functions
+library(dplyr)
+library(purrr)
+
+# Visualization
+library(gt)
+library(ggplot2)
+library(showtext)
+library(see) # half-violin plots
+library(ggiraph)
+library(htmltools)
